c66f6707a4e11852e307a0f0b34168f5c1f1dda2
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-05-03 18:02:52 +0530

    Delete Trinker directory

--- Trinker/trinker tutorial 1.py
+++
@@ -1,7 +0,0 @@
-# how use trinker
-
-import turtle
-
-misterparas = turtle.Turtle()
-
-misterparas.forward(200)
--- Trinker/trinker tutorial 2.py
+++
@@ -1,9 +0,0 @@
-#adding dicrection
-
-import turtle
-
-paras = turtle.Turtle()
-
-paras.setheading(45)
-
-paras.forward(100)
