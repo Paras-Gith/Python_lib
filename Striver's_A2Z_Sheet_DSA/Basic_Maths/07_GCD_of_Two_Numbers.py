245537d2dc4bd7dd8f69e60b60f1301ead254571
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-05-25 05:13:26 +0530

    Add files via upload

---
+++ Striver's_A2Z_Sheet_DSA/Basic_Maths/07_GCD_of_Two_Numbers.py
@@ -0,0 +1,14 @@
+def GCD(n1, n2):
+    gcd = 1
+    for i in range(1, min(n1,n2)+1):
+        if n1%i==0 and n2%i==0:
+            gcd = i
+    return gcd
+
+def main():
+    n1 = int(input("Enter first number: "))
+    n2 = int(input("Enter second number: "))
+    print(GCD(n1, n2))
+
+if __name__ == "__main__":
+    main()
